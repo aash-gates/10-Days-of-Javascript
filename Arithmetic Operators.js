664cc493dca8c3d58dfe37febec9cd0edddbfca8
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-03 10:01:04 +0530

    Update Arithmetic Operators.js

--- Arithmetic Operators.js
+++ Arithmetic Operators.js
@@ -1,3 +1,26 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
 /**
 *   Calculate the area of a rectangle.
 *
@@ -22,4 +45,12 @@ function getPerimeter(length, width) {
     var perimeter = length * 2 + width * 2;
     
     return perimeter;
+}
+
+function main() {
+    const length = +(readLine());
+    const width = +(readLine());
+    
+    console.log(getArea(length, width));
+    console.log(getPerimeter(length, width));
 }
