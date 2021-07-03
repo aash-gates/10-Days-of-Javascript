4c6e1724f6f5151227d8290d59a4d22fe2562aa2
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-03 09:53:28 +0530

    Update Inheritance.js

--- Inheritance.js
+++ Inheritance.js
@@ -1,3 +1,10 @@
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+}
+
 /*
  *  Write code that adds an 'area' method to the Rectangle class' prototype
  */
@@ -12,4 +19,16 @@ class Square extends Rectangle {
     constructor(side) {
         super(side, side);
     }
+}
+
+
+if (JSON.stringify(Object.getOwnPropertyNames(Square.prototype)) === JSON.stringify([ 'constructor' ])) {
+    const rec = new Rectangle(3, 4);
+    const sqr = new Square(3);
+    
+    console.log(rec.area());
+    console.log(sqr.area());
+} else {
+    console.log(-1);
+    console.log(-1);
 }
