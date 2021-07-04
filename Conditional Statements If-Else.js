ef5af5b0d7b32d8686e88e1c152296f3c8afb1f2
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-04 21:13:47 +0530

    Update Conditional Statements If-Else.js

--- Conditional Statements If-Else.js
+++ Conditional Statements If-Else.js
@@ -1,15 +1,11 @@
 'use strict';
-
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
-
 let inputString = '';
 let currentLine = 0;
-
 process.stdin.on('data', inputStdin => {
     inputString += inputStdin;
 });
-
 process.stdin.on('end', _ => {
     inputString = inputString.trim().split('\n').map(string => {
         return string.trim();
@@ -17,26 +13,27 @@ process.stdin.on('end', _ => {
     
     main();    
 });
-
 function readLine() {
     return inputString[currentLine++];
 }
-
 function getGrade(score) {
-    let grade;
-    switch (s.charAt(0)) {
-    case 'a':
-        console.log('A');
-        break;
-    case 'b':
-        console.log('A');
-        break;
-    ...
-}
+    // Write your code here
+    if(score > 25 && score <= 30){
+        return("A");
+    }else if(score > 20 && score <= 25){
+        return("B");
+    }else if(score > 15 && score <= 20){
+        return("C");
+    }else if(score > 10 && score <= 15){
+        return("D");
+    }else if(score > 5 && score <= 10){
+        return("E");
+    }else if(score > 0 && score <= 5){
+        return("F");
+    }
+    
     return grade;
 }
-
-
 function main() {
     const score = +(readLine());
     
