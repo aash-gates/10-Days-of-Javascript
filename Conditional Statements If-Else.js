51294f56e019cf152122a072e79394999c47a871
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-04 21:13:41 +0530

    Update Conditional Statements If-Else.js

---
+++ Conditional Statements If-Else.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getGrade(score) {
+    let grade;
+    switch (s.charAt(0)) {
+    case 'a':
+        console.log('A');
+        break;
+    case 'b':
+        console.log('A');
+        break;
+    ...
+}
+    return grade;
+}
+
+
+function main() {
+    const score = +(readLine());
+    
+    console.log(getGrade(score));
+}
