4d55b005282f8566ecc8235a129177714cab4262
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-05 14:07:44 +0530

    Update Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -1,36 +1,66 @@
-/**
-*   Needed for Arrays.sort() to sort numerically instead of lexicographically.
-*   (numerical sorting = 1, 2, 10; lexicographical sorting = 1, 10, 2)
-*   @param {Number} a 
-*   @param {Number} b
-*   @return {Number} The value of a - b.
-**/
-function comparator(a, b) {
-    return a - b;
+
+Homecoding problemsHackerRank Day 3: Arrays 10 days of javascript solution
+HackerRank Day 3: Arrays 10 days of javascript solution
+YASH PAL February 07, 2021
+
+In this HackerRank Day 3: Arrays 10 days of javascript problem Complete the getSecondLargest function in the editor below. It has one parameter: an array, nums, of n numbers. The function must find and return the second largest number in nums.
+HackerRank Day 3: Arrays 10 days of javascript solution
+
+
+HackerRank Day 3: Arrays 10 days of javascript problem solution.
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
 }
 
 /**
 *   Return the second largest number in the array.
-*   @param {Number[]} nums
+*   @param {Number[]} nums - An array of numbers.
 *   @return {Number} The second largest number in the array.
 **/
 function getSecondLargest(nums) {
-    // Sort the array in ascending order
-    nums.sort(comparator);
-    const max = nums[nums.length - 1];
-
-    // Find the index of the second largest value
-    // Set index to end of array
-    let index = nums.length - 1;
-    // Loop through array from back to front, decrementing index before each iteration
-    while (index--) {
-        // Return the first integer that is not the maximum 
-        // This is guaranteed to be the second-largest value because the array is sorted
-        if (nums[index] != max) {
-            return nums[index];
-        }
+    // Complete the function
+   let first = nums[0]; 
+   let second = -1;
+    for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > first) {
+        second = first;
+        first = nums[i]
     }
+
+    if (nums[i] > second && nums[i] < first) {
+        second = nums[i];
+    }
+}
+
+
+return second;
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
     
-    // Error: This should only ever be reached if all values in the array are equal.
-    return max;
+    console.log(getSecondLargest(nums));
 }
